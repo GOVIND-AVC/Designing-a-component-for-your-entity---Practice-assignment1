7fe319e85d70e4b2dc811720e07ebef429cfc474
GOVIND AVC <eemail2govindavc@gmail.com>
2025-03-04 14:23:11 +0530

    added changes:

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import React from "react";
+import Usercard from "./components/usercard";
+
+const App = () => {
+  return (
+    <div>
+      <Usercard
+        name="BRUCE"
+        profilephoto="https://free-3dtextureshd.com/wp-content/uploads/2024/06/26.jpg.webp"
+        mail="wayne007ent@gmail.com"
+        phone="911"
+        address="goutham city"
+      />
+    </div>
+  );
+};
+
+export default App;
